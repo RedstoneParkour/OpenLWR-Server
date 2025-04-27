e1a4c49b8033a72ca7c82a110182ec813389140c
obamacode <74860621+SakuruWasTaken@users.noreply.github.com>
2025-04-27 05:54:46 +0000

    Fix dev_test model

--- simulation/models/dev_test/model.py
+++ simulation/models/dev_test/model.py
@@ -2,7 +2,14 @@ from simulation.constants.annunciator_states import AnnunciatorStates
 import math
 
 alarms = {
-    "test_alarm" : AnnunciatorStates.CLEAR,
+    "test_alarm" : {
+        "alarm" : False,
+        "box": "Box1",
+		"window": "1-2",
+        "state" : AnnunciatorStates.CLEAR,
+        "group" : "1",
+        "silenced" : False,
+    },
 }
 
 switches = {
@@ -13,6 +20,12 @@ switches = {
 			2: -45,
 		},
         "position": 0,
+        "lights" : {     
+            "green" : True,
+            "red" : False,
+        },
+        "flag" : "green",
+        "momentary" : False,
     },
     "test_switch2": {
         "positions": {
@@ -21,6 +34,12 @@ switches = {
 			2: -45,
 		},
         "position": 0,
+        "lights" : {     
+            "green" : True,
+            "red" : False,
+        },
+        "flag" : "green",
+        "momentary" : False,
     },
 }
 
@@ -36,6 +55,8 @@ buttons = {
     "test_button": False
 }
 
+recorders = {}
+
 test_value = 0
 
 def model_run(delta):
@@ -46,8 +67,11 @@ def model_run(delta):
         test_value += 0.1
     values["test_gauge"] = math.sin(test_value) / 2 + 0.5
     indicators["lamp_test"] = switches["test_switch"]["position"] != 1
-    alarms["test_alarm"] = AnnunciatorStates.CLEAR
+    alarms["test_alarm"]["state"] = AnnunciatorStates.CLEAR
     if switches["test_switch"]["position"] == 0:
-        alarms["test_alarm"] = AnnunciatorStates.ACTIVE
+        alarms["test_alarm"]["state"] = AnnunciatorStates.ACTIVE
     elif switches["test_switch"]["position"] == 2:
-        alarms["test_alarm"] = AnnunciatorStates.ACTIVE_CLEAR
+        alarms["test_alarm"]["state"] = AnnunciatorStates.ACTIVE_CLEAR
+
+def model_run_fast(delta):
+    pass
