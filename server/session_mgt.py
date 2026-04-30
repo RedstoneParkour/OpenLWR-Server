e13632f6812f2d60589e52976f0cd99947dbeccf
watchful5406 <seantyler525@gmail.com>
2026-04-29 18:56:42 -0700

    finish handshake, properly assign session ids

--- server/session_mgt.py
+++ server/session_mgt.py
@@ -23,6 +23,8 @@ class SessionManager:
         self.OnInteraction = Events() #go to devices
         self.OnEvent = Events() #go somewhere idk
 
+        self.counter_sessionid = 0
+
     def ProcessDataRec(self,data,address):
 
         recmessage = rec_proto.RECMessage()
@@ -50,7 +52,16 @@ class SessionManager:
 
             #TODO: verification types for password access
             case rec_proto.RECMessageType.REC_HANDSHAKE:
-                client.OnHandshake(recmessage)
+                if client.OnHandshake(recmessage,self.counter_sessionid):
+
+                    ClientSession = self.SessionRegistry.Find(address)
+                    ClientSession.Username = recmessage.handshake.username
+                    ClientSession.RecSession.MinorVersion = recmessage.handshake.client_minor_version
+
+                    ClientSession.RecSession.State = SessionState.Active #declare connection active
+                else:  #if we reject them, we just delete their session
+                    self.SessionRegistry.Remove(self.SessionRegistry.Find(address).RecSession.SessionId)
+
             
             case rec_proto.RECMessageType.REC_EVENT:
                 self.OnEvent.on_changed(recmessage)
@@ -70,7 +81,8 @@ class SessionManager:
                 #create a session and rec connection, and register that with session manager
                 print("servicing new connection") #TODO: this is a temporary print, we can remove this later
 
-                rec_connection = RecConnection(ClientAddress=address,Client=connection) # we will edit all of this later when we handshake
+                rec_connection = RecConnection(ClientAddress=address,Client=connection,SessionId=self.counter_sessionid) # we will edit all of this later when we handshake
+                self.counter_sessionid += 1 #this will never decrease
                 user_session = Session(rec_connection)
                 self.SessionRegistry.Add(user_session)
 
@@ -107,7 +119,16 @@ class Connection:
 
     def Send(self,message):
         pass
-    
+
+class UbcConnection(Connection):
+    def __init__(self,SessionId:np.uint32,MinorVersion:np.uint32,State:SessionState,ClientAddress:tuple):
+        super().__init__(SessionId,MinorVersion,State,ClientAddress)
+
+    def Subscribe(connection:Connection):
+        pass
+
+    def Unsubscribe(connection:Connection):
+        pass
 
 class RecConnection(Connection): 
     def __init__(self,Client,SessionId:np.uint32=-1,MinorVersion:np.uint32=-1,State:SessionState=SessionState.Connecting,ClientAddress:tuple=None):
@@ -115,10 +136,11 @@ class RecConnection(Connection):
 
         self.Client = Client #we have to have this because tcp is special i guess
 
-    def OnHandshake(self,message):
-        Status,msg = rec_communication.RecHandshake(message,session_id=1) #temporary magic number
+    def OnHandshake(self,message,session_id):
+        Status,msg = rec_communication.RecHandshake(message,session_id) #temporary magic number
 
         self.Send(msg)
+        return Status == RecStatus.OK
 
     def Send(self,message):
         self.Client.send(message.SerializeToString())
@@ -132,14 +154,19 @@ class Session:
         self.Username = Username
 
     def IsThisMyClient(self,ClientAddress:tuple):
-        if self.RecSession.IsThisMyClient(ClientAddress):
-            return True, self.RecSession
+        if self.RecSession != None:
+            if self.RecSession.IsThisMyClient(ClientAddress):
+                return True, self.RecSession
         
-        if self.UbcSession.IsThisMyClient(ClientAddress):
-            return True, self.UbcSession
+        if self.UbcSession != None:
+            if self.UbcSession.IsThisMyClient(ClientAddress):
+                return True, self.UbcSession
         
-        if self.UecSession.IsThisMyClient(ClientAddress):
-            return True, self.UecSession
+        if self.UecSession != None:
+            if self.UecSession.IsThisMyClient(ClientAddress):
+                return True, self.UecSession
+            
+        return False, None
 
     
 
@@ -158,3 +185,10 @@ class SessionRegistry:
 
     def Remove(self,sessionId:np.uint32):
         self.sessions.pop(sessionId)
+
+    def Find(self,address:tuple):
+        for v in self.sessions:
+            IsSession, Session = self.sessions[v].IsThisMyClient(address)
+
+            if IsSession:
+                return self.sessions[v]
