9c161f0ce367137d02ce082cf65006c710cc7155
watchful5406 <seantyler525@gmail.com>
2026-05-03 15:52:36 -0700

    update protobuf, add indicators

--- server/example/simulation_module.py
+++ server/example/simulation_module.py
@@ -9,8 +9,10 @@ class SBMBreaker(devices.DeviceBase):
     def on_interaction(self, interaction_id, interaction_type, data):
         self.set_field_value(data.field,getattr(data,data.WhichOneof("data")))
 
-
-
+class Indicator(devices.DeviceBase):
+    def __init__(self, name: str):
+        super().__init__(device_type="Indicator",name=name)
+        self._fields = [devices.DeviceField("Lit",0,False)]
 
 
 
@@ -18,14 +20,24 @@ class BlinkenLights(simulation.SimulationModule):
     def __init__(self):
         super().__init__()
         self.LPCS_Pump = SBMBreaker("LPCS_Pump")
+        self.LPCS_Off = Indicator("LPCS_Off")
+        self.LPCS_On = Indicator("LPCS_On")
         devices.REGISTRY.add_device(self.LPCS_Pump)
+        devices.REGISTRY.add_device(self.LPCS_Off)
+        devices.REGISTRY.add_device(self.LPCS_On)
+        self.PumpState = False
 
     def OnTick(self,world:simulation.SimulationContext,ctx:simulation.TickContext):
 
         #print(f"executed. dt:{ctx.Delta} elapsed:{ctx.Elapsed} ")
-        #store some data
-        print(devices.REGISTRY.get_device(self.LPCS_Pump.device_id).get_field_by_id(0))
-        self.LPCS_Pump.set_field_value(0, 1)
+        if devices.REGISTRY.get_device(self.LPCS_Pump.device_id).get_field_by_id(0).value == 2:
+            self.PumpState = True
+        elif devices.REGISTRY.get_device(self.LPCS_Pump.device_id).get_field_by_id(0).value == 0:
+            self.PumpState = False
+
+        self.LPCS_Off.set_field_value(0, not self.PumpState)
+        self.LPCS_On.set_field_value(0, self.PumpState)
+
 
 
 
